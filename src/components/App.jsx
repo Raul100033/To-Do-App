2fab24933dc6fec3a42dcfd14ac953440c46c4ec
Raul Orbe Rivera <raulorberivera@gmail.com>
2021-03-01 21:33:16 -0600

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  const addItem = (inputText) => {
+    setItems(prevItems => [...prevItems, inputText]);
+  }
+
+  function deleteItem(id) {
+    setItems(prevItems => prevItems.filter((item, index) => index !== id));
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea onAdd={addItem} />
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
